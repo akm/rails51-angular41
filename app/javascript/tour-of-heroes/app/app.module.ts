1206475a855a56fb3f5634794268c67ea8ec7c7f
akm <akm2000@gmail.com>
2017-05-06 18:02:14 +0900

    Import APP_BASE_HREF to avoid the following error:
    Unhandled Promise rejection: No base href set. Please provide a value for the APP_BASE_HREF token or add a base element to the document. ; Zone: <root> ; Task: Promise.then ; Value: Error: No base href set. Please provide a value for the APP_BASE_HREF token or add a base element to the document.
    
    See http://stackoverflow.com/questions/40274540/chrome-extension-no-base-href-set-please-provide-a-value-for-the-app-base-hre

--- app/javascript/tour-of-heroes/app/app.module.ts
+++ app/javascript/tour-of-heroes/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -32,7 +33,8 @@ import { HeroSearchComponent } from './hero-search.component';
         HeroSearchComponent
     ],
     providers: [
-        HeroService
+        HeroService,
+        { provide: APP_BASE_HREF, useValue: '/heroes/index' }
     ],
     bootstrap:    [ AppComponent ]
 })
